591283178e318d21987bed74166a45903a648618
rushabh1191 <rushabhb@gsteckno.com>
2016-11-05 18:33:20 +0530

    initial commit

---
+++ app/src/main/java/com/rushabh/subreddit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rushabh.subreddit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
